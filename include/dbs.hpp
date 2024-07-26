2b9b5835f8a241e75fc3bc1bfe2be3958ca8cd8d
Heisenberg <alimirmohammad.1386@gmail.com>
2024-07-26 10:29:00 -0400

    update

--- include/dbs.hpp
+++ include/dbs.hpp
@@ -36,4 +36,37 @@ class Database{
         }
 };
 
+namespace DBS{
+    int chooseOptions(){
+        int result;
+        std::cout << "Choose option: " << std::endl;
+        std::cout << "1. Create table" << std::endl;
+        std::cout << "2. Insert data" << std::endl;
+        std::cout << "3. Select data" << std::endl;
+        std::cout << "4. Update data" << std::endl;
+        std::cout << "5. Delete data" << std::endl;
+        std::cout << "6. Exit" << std::endl;
+        
+        std::cin >> result;
+
+        switch(result){
+            case 1:
+                return 1;
+            case 2:
+                return 2;
+            case 3:
+                return 3;
+            case 4:
+                return 4;
+            case 5:
+                return 5;
+            case 6:
+                return 6;
+            default:
+                return -1;
+        }
+
+    }
+}
+
 #endif
